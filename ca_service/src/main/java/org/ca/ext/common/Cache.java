0c213cff78be27e516249bd1d3c010ee839bf84b
ligson <lijinsheng@lecxe.com>
2016-05-19 09:57:16 +0800

    把ext代码放到common中

--- ca_service/src/main/java/org/ca/ext/common/Cache.java
+++
@@ -1,9 +0,0 @@
-package org.ca.ext.common;
-
-import java.lang.ref.ReferenceQueue;
-
-public class Cache {
-public static void main(String[] args) {
-	ReferenceQueue queue;
-}
-}
--- ca_service/src/main/java/org/ca/ext/security/tsp/TSPExtensions.java
+++
@@ -1,7 +0,0 @@
-package org.ca.ext.security.tsp;
-
-import org.ca.ext.security.x509.X509Extensions;
-
-public class TSPExtensions extends X509Extensions{
-
-}
